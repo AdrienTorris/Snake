7181b7ac94a23045b0295d9b75803166c2b8943f
Adrien Torris <adrientorris@gmail.com>
2016-12-31 16:56:44 +0100

    style and package info

--- src/Snake.Web/wwwroot/js/snake.js
+++ src/Snake.Web/wwwroot/js/snake.js
@@ -1,8 +1,26 @@
+/**
+ * Size of elements on the map
+ */
 const SIZE = 20;
+/**
+ * Width of the map
+ */
 const GAME_WRAPPER_WIDTH = 600;
+/**
+ * Height of the map
+ */
 const GAME_WRAPPER_HEIGHT = 300;
+/**
+ * Top margin of the map
+ */
 const GAME_WRAPPER_TOP = 150;
+/**
+ * Left margin of the map
+ */
 const GAME_WRAPPER_LEFT = 20;
+/**
+* List of keyboard keys the game handle
+*/
 var keys;
 (function (keys) {
     keys[keys["RETURN"] = 13] = "RETURN";
@@ -16,8 +34,17 @@ var keys;
     keys[keys["J"] = 74] = "J";
     keys[keys["K"] = 75] = "K";
 })(keys || (keys = {}));
+/**
+ * Utility methods to help dealing with some stuff
+ */
 var Utils;
 (function (Utils) {
+    /**
+     * Get a random number
+     * @param min
+     * @param max
+     * @param reduce
+     */
     function rand(min, max, reduce = SIZE) {
         let num = Math.floor(Math.random() * (max - min)) + min;
         return num - (num % reduce);
@@ -59,57 +86,86 @@ var Directions;
     }
     Directions.peek = peek;
 })(Directions || (Directions = {}));
+/**
+ * Game
+ */
 class Game {
+    /**
+     * New instance of the game
+     */
     constructor() {
+        /**
+        * User score
+        */
         this.score = 0;
+        /**
+        * Snake is moving
+        */
         this.moving = false;
         /**
         * Snake body size
         */
         this.length = 0;
+        // Build the map
         this.gameWrapper = document.getElementById('game_wrapper');
         this.gameWrapper.style.width = `${GAME_WRAPPER_WIDTH}px`;
         this.gameWrapper.style.height = `${GAME_WRAPPER_HEIGHT}px`;
         this.gameWrapper.style.top = `${GAME_WRAPPER_TOP}px`;
         this.gameWrapper.style.left = `${GAME_WRAPPER_LEFT}px`;
+        // Create the snake
         this.head = new Piece(this.gameWrapper, SIZE * 3, SIZE * 3, "head");
+        // Create map's walls
         this.addWalls();
+        // Create a food to feed the snake with
         this.handleFood();
+        // Events the game handles
         this.setEvents();
     }
+    /**
+     * Add the walls the snake can't hurt
+     */
     addWalls() {
-        // mur gauche
+        // left
         for (var _i = 0; _i < GAME_WRAPPER_HEIGHT; _i++) {
             let mur = new Piece(this.gameWrapper, 0, _i, 'wall');
             _i = _i + SIZE - 1;
         }
-        // mur droit
+        // right
         for (var _i = 0; _i < GAME_WRAPPER_HEIGHT; _i++) {
             let mur = new Piece(this.gameWrapper, GAME_WRAPPER_WIDTH - SIZE, _i, 'wall');
             _i = _i + SIZE - 1;
         }
-        // mur haut
+        // top
         for (var _i = SIZE; _i < GAME_WRAPPER_WIDTH - SIZE; _i++) {
             let mur = new Piece(this.gameWrapper, _i, 0, 'wall');
             _i = _i + SIZE - 1;
         }
-        // mur bas
+        // bottom
         for (var _i = SIZE; _i < GAME_WRAPPER_WIDTH - SIZE; _i++) {
             let mur = new Piece(this.gameWrapper, _i, GAME_WRAPPER_HEIGHT - SIZE, 'wall');
             _i = _i + SIZE - 1;
         }
     }
+    /**
+     * Start a game
+     */
     start() {
         this.showScore();
         this.moving = true;
         requestAnimationFrame(this.frame.bind(this));
     }
+    /**
+     * Display the user score
+     */
     showScore() {
         let el = document.querySelector(".score");
         el.innerHTML = `
             Score: ${this.score}
         `;
     }
+    /**
+     * Calculate the score
+     */
     updateScore() {
         return this.score += 1;
     }
@@ -117,37 +173,42 @@ class Game {
      * Get a random empty location for food
      */
     getFoodLocation() {
+        // Generate random x and y positions
         let x = Utils.rand(GAME_WRAPPER_LEFT, GAME_WRAPPER_WIDTH, SIZE);
         let y = Utils.rand(GAME_WRAPPER_TOP, GAME_WRAPPER_HEIGHT, SIZE);
-        // If random spot is already filled, pick a new one
-        // Pick until you find an empty spot
-        // ..... nothing can go wrong with this
+        // If random spot is already filled, pick a new one to avoid conflicts
         if (Locations.has(x, y)) {
             [x, y] = this.getFoodLocation();
         }
         return [x, y];
     }
+    /**
+     * Handle food => Create a new one if there is no food on the map, and manage snake/food collision
+     */
     handleFood() {
+        // If no food on the map, create a new one, snake have to eat !
         if (this.food == null) {
             let [foodX, foodY] = this.getFoodLocation();
             this.food = new Piece(this.gameWrapper, foodX, foodY, "food");
         }
-        // if head and food collided, replace head with the food
-        // set the correct type for each piece
+        // Manage a collision between the snake and a food item
         if (this.head.x === this.food.x && this.head.y === this.food.y) {
-            this.food.next = this.head; // put food at the top of the chain
-            this.food.direction = this.head.direction; // Needs to go to same direction where head was going
-            this.head.setType("body"); // head is not body
-            this.food.setType("head"); // food is now head
-            this.head = this.food; // Update the Game instance with new head
-            this.food = null; // food is gone now
+            this.food.next = this.head;
+            this.food.direction = this.head.direction;
+            this.head.setType("body");
+            this.food.setType("head");
+            this.head = this.food;
+            this.food = null;
+            // Grow up the size of the snake
             this.length++;
-            this.updateScore(); // Calculate the new score
-            this.showScore(); // Update the score
+            // Update and display new score
+            this.updateScore();
+            this.showScore();
         }
     }
     /**
-     * Don"t let snake to go backwards
+     * Manage the thing that the snake can't move backward
+     * @param key
      */
     notBackwards(key) {
         let lastDirection = Directions.peek();
@@ -159,10 +220,12 @@ class Game {
         }
         return true;
     }
+    /**
+     * Set events the game handles
+     */
     setEvents() {
         document.addEventListener("keydown", (e) => {
             switch (e.keyCode) {
-                // Arrow keys or nothing
                 default:
                     if (e.keyCode in keys && this.notBackwards(e.keyCode)) {
                         Directions.set(e.keyCode);
@@ -170,6 +233,9 @@ class Game {
                     }
             }
         });
+        /**
+        * User clicked on the start game button
+        */
         document.addEventListener("click", (e) => {
             let el = e.target;
             if (el.id === "start") {
@@ -178,7 +244,7 @@ class Game {
         });
     }
     /**
-     * GAME OVER
+     * Game is over
      */
     over() {
         this.moving = false;
@@ -193,11 +259,9 @@ class Game {
                 requestAnimationFrame(this.frame.bind(this));
             }, this.getSpeed());
         }
-        // If head hits an occupied space, GAME OVER
         if (Locations.has(this.head.x, this.head.y)) {
             return this.over();
         }
-        // If Game is not over, then move the snake to requested direction
         let direction = Directions.pop();
         if (direction === keys.RIGHT) {
             this.head.move(this.head.x + SIZE, this.head.y, keys[direction]);
@@ -211,30 +275,55 @@ class Game {
         if (direction === keys.UP) {
             this.head.move(this.head.x, this.head.y - SIZE, keys[direction]);
         }
-        // Check if we caught caught the food
-        // or we need to place a new food
         this.handleFood();
     }
+    /**
+     * Get the speed of the game
+     */
     getSpeed() {
         return 150;
     }
 }
+/**
+ * Handle item locations
+ */
 var Locations;
 (function (Locations) {
+    /**
+     * Filled location collection
+     */
     let data = {};
+    /**
+     * Set a new location
+     * @param x
+     * @param y
+     */
     function set(x, y) {
         data[`${x}:${y}`] = true;
     }
     Locations.set = set;
+    /**
+     * Remove a location
+     * @param x
+     * @param y
+     */
     function remove(x, y) {
         delete data[`${x}:${y}`];
     }
     Locations.remove = remove;
+    /**
+     * Check if a location is filled
+     * @param x
+     * @param y
+     */
     function has(x, y) {
         return data[`${x}:${y}`] === true;
     }
     Locations.has = has;
 })(Locations || (Locations = {}));
+/**
+ * Game's item
+ */
 class Piece {
     constructor(gameWrapper, x, y, type = "body", direction = "RIGHT") {
         this.gameWrapper = gameWrapper;
@@ -257,52 +346,35 @@ class Piece {
         this.el.classList.add("cell", this.type, this.direction);
     }
     setPos(x, y) {
-        // CSS nove the element
         this.el.style.top = `${y}px`;
         this.el.style.left = `${x}px`;
-        // this.el.style.transform = `translate(${x}px, ${y}px)`;
-        // reset CSS classnames basically
         this.applyClass();
-        // Save the location of this piece to occupied spaces
-        // But don"t do this, if we are the food or head because;
-        // - Head cannot collide with itself
-        // - We want to collide with food :)
         if (this.type !== "head" && this.type !== "food") {
             Locations.set(x, y);
         }
     }
     move(x, y, direction = "RIGHT") {
         this.direction = direction;
-        // Move HTML Element to new spot
         this.setPos(x, y);
-        // Save the old direction
         let tdirection = this.direction;
-        // Set new direction of the piece
         this.direction = direction;
-        // If there is a next piece move it to old position
         if (this.next !== null) {
-            // If this piece is a head piece, immediate piece should receive heads current
-            // direction instead of old one this is needed to have a fluid motion
             this.next.move(this.x, this.y, this.type === "head" ? this.direction : tdirection);
         }
         else {
-            // We are the last piece, previous position
-            // is now empty, clear it
             Locations.remove(this.x, this.y);
         }
-        // if I"m part of body and no one is following me
-        // then I must be the tail
         if (this.next === null && this.type === "body") {
             this.el.classList.add("tail");
         }
-        // if me and the piece following me are at the same spot
-        // then piece following me must be the food we just swallowed
         if (this.next !== null && this.next.x === x && this.next.y === y) {
             this.next.el.classList.add("gulp");
         }
-        // store new values
         this.x = x;
         this.y = y;
     }
 }
+/**
+ * Instanciate a new game
+ */
 let g = new Game();
